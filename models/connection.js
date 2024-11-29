3a72c922c0c3ccb724b59da56e0fc4c106b9f03d
Pierre <chabertpierre13@gmail.com>
2024-11-29 15:49:35 +0100

    save

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = process.env.REACT_APP_CONNECTION_STRING;
 
 mongoose
 	.connect(connectionString, { connectTimeoutMS: 2000 })
